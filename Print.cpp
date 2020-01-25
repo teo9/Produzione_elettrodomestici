1d3dc6614139d92a42df12b1e0637cf03717916e
riccardo <31276674+xRowie@users.noreply.github.com>
2020-01-25 19:24:48 +0100

    file stampa

---
+++ Print.cpp
@@ -0,0 +1,7 @@
+/** file di stampa */
+
+/** acquisti effettuati (componenti acquistati ma non ancora arrivati) */
+
+/** magazzino (componenti ordinati e arrivati, ma non ancora utilizzati per la produzione) */
+
+/** ordini evasi (modelli prodotti e consegnati) */
