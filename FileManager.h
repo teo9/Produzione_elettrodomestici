d92fb9f638d9107e612f4ff68f91b4da0dc8fd49
riccardo <31276674+xRowie@users.noreply.github.com>
2020-01-25 20:15:12 +0100

    gestore dei file

---
+++ FileManager.h
@@ -0,0 +1,5 @@
+/** classe che legge i dati dai file e li interpreta */
+
+/** leggiamo per ogni mese gli ordini che ci sono, prendiamo i modelli e
+    dunque i componenti necessari per costruirlo
+*/
