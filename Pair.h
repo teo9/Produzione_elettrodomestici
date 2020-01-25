5f31574360ec646d64ba7f9ab80a9a78e36fdf45
riccardo <31276674+xRowie@users.noreply.github.com>
2020-01-25 12:01:40 +0100

    hhhh

---
+++ Pair.h
@@ -0,0 +1,17 @@
+/** Riccardo Rampon - 1195597 */
+
+template<typename T, int Q>
+class Pair{
+	public:
+		Pair(T tClass, int tQuantity);
+		const T& getClass() const;
+		const int& getQuantity() const;
+		void setQuantity(int nQ);
+		bool operator==(const Pair<T,Q>& pair) const;
+		bool operator!=(const Pair<T,Q>& pair) const;
+		Pair<T,Q>& operator=(const Pair<T,Q>& pair);
+
+	private:
+		T tClass;
+		int tQuantity;
+};
