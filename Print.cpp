96cefd81c0d11a1af0d1a4ce59cf22a9afb3c139
riccardo <31276674+xRowie@users.noreply.github.com>
2020-01-27 19:33:08 +0100

    uuu

--- Print.cpp
+++ Print.cpp
@@ -1,9 +1,6 @@
 /** Rampon Riccardo - 1195597 */
 #include "OrdersManager.h"
 
-	/*vector<Order> delivered_orders; //ordini evasi
-		vector<Components> warehouse; //magazzino(componenti ordinati e arrivati)*/
-
 /** get_acquired_components() */
 void OrdersManager::get_acquired_components(){
 	for(int i=0; i<acquired_components.size(); i++)
@@ -15,10 +12,14 @@ void OrdersManager::get_acquired_components(){
 
 /** get_warehouse() */
 void OrdersManager::get_warehouse(){
-
+	for(int i=0; i<warehouse.size(); i++)
+		Component comp= warehouse[i].getComp();//prende il riferimento this di Components
+    	std::cout << "\nComponent_Id: " << comp[i].getId() << "\nComponent_Name: " << comp[i].getName() << "Component_Quantity: " << comp[i].getName();
 }//get_warehouse
 
 /** get_delivered_orders() */
 void OrdersManager::get_delivered_orders(){
-
+	for(int i = 0; i<delivered_orders.size(); i++){
+        Model m = delivered_orders[i].getModel();
+        std::cout << "\nMonth: " << delivered_orders[i].getMonth() << "| Order Id: " << delivered_orders[i].getIdOrder() << "| Model Id: " << m.getId() << "| Model Name: " << m.getName() << "| Quantity: " << delivered_orders[i].getQuantity();
 }//get_delivered_orders
