bc4a0265005a91fd0d49d6d0e468a50853607a17
riccardo <31276674+xRowie@users.noreply.github.com>
2020-01-27 19:02:57 +0100

    ghjghj

--- Print.cpp
+++ Print.cpp
@@ -1,9 +1,24 @@
-#include "UtilityOrders.h"
+/** Rampon Riccardo - 1195597 */
+#include "OrdersManager.h"
 
-/** file di stampa */
+	/*vector<Order> delivered_orders; //ordini evasi
+		vector<Components> warehouse; //magazzino(componenti ordinati e arrivati)*/
 
-/** acquisti effettuati (componenti acquistati ma non ancora arrivati) */
+/** get_acquired_components() */
+void OrdersManager::get_acquired_components(){
+	for(int i=0; i<acquired_components.size(); i++)
+		Model m= (acquired_components)[i].getModel();
+		vector<Components> comp= m.getComponents();
+		for(int j=0; j<comp.size(); j++)
+			std::cout << "\nComponent_Id: " << comp[j].getId() << "\nComponent_Name: " << comp[j].getName() << "Component_Quantity: " << comp[j].getName();
+}//get_acquired_components
 
-/** magazzino (componenti ordinati e arrivati, ma non ancora utilizzati per la produzione) */
+/** get_warehouse() */
+void OrdersManager::get_warehouse(){
 
-/** ordini evasi (modelli prodotti e consegnati) */
+}//get_warehouse
+
+/** get_delivered_orders() */
+void OrdersManager::get_delivered_orders(){
+
+}//get_delivered_orders
