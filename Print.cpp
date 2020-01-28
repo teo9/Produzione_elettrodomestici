fc353338ed9cc40c09030c502ac17c747bb240de
riccardo <38914771+rikyyy@users.noreply.github.com>
2020-01-28 21:28:06 +0100

    sdsd

--- Print.cpp
+++ Print.cpp
@@ -1,25 +1,25 @@
 /** Rampon Riccardo - 1195597 */
-#include "OrdersManager.h"
+#include "Azienda.h"
 
 /** get_acquired_components() */
-void OrdersManager::get_acquired_components(){
-	for(int i=0; i<acquired_components.size(); i++)
-		Model m= (acquired_components)[i].getModel();
+void Azienda::get_acquired_component(){
+	for(int i=0; i<comp_acquistati.size(); i++)
+		Model m= (comp_acquistati)[i].getModel();
 		vector<Components> comp= m.getComponents();
 		for(int j=0; j<comp.size(); j++)
-			std::cout << "\nComponent_Id: " << comp[j].getId() << "\nComponent_Name: " << comp[j].getName() << "Component_Quantity: " << comp[j].getName();
+			//stampa id, nome, .... std::cout <<
 }//get_acquired_components
 
 /** get_warehouse() */
-void OrdersManager::get_warehouse(){
-	for(int i=0; i<warehouse.size(); i++)
-		Component comp= warehouse[i].getComp();//prende il riferimento this di Components
-    	std::cout << "\nComponent_Id: " << comp[i].getId() << "\nComponent_Name: " << comp[i].getName() << "Component_Quantity: " << comp[i].getName();
+void Azienda::get_warehouse(){
+	for(int i=0; i<magazzino.size(); i++)
+		Components comp= warehouse[i].getComp();
+		//stampa id, nome, ... std::cout <<
 }//get_warehouse
 
 /** get_delivered_orders() */
-void OrdersManager::get_delivered_orders(){
-	for(int i = 0; i<delivered_orders.size(); i++){
-        Model m = delivered_orders[i].getModel();
-        std::cout << "\nMonth: " << delivered_orders[i].getMonth() << "| Order Id: " << delivered_orders[i].getIdOrder() << "| Model Id: " << m.getId() << "| Model Name: " << m.getName() << "| Quantity: " << delivered_orders[i].getQuantity();
+void Azienda::get_delivered_orders(){
+	for(int i=0; i<ordini_evasi.size(); i++)
+		Model m= (ordini_evasi)[i];
+		//stampa id, nome, .... std::cout <<	
 }//get_delivered_orders
