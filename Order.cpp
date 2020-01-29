02dc77555ad425d407f9d9834a542b2a77fec5f4
Alberto99524 <60016888+Alberto99524@users.noreply.github.com>
2020-01-29 21:46:04 +0100

    uppato

--- Order.cpp
+++ Order.cpp
@@ -1,30 +1,28 @@
 //Alberto Cappelletto 1196055
 
 #include<iostream>
-#include"Order.h"
-#include"Model.h"
+#include"Header.h"
 
-using namespace std;
+using namespace std;						
 
-Order::Order() {					//costruttore di default
+Ordine::Ordine() {							//costruttore di default
 	time = 0;
 	id = 0;
 	quantity = 0;
 }
-Order::Order(int t,int k,int n) {			//costruttore di parametri
+
+Ordine::Ordine(int t,int k,int n) {
 	time = t;
 	id = k;
 	quantity = n;
 }
-/*Order::Order(string s) {				//costruttore per il load del file
-	//load file
-}*/
-Order::Order(const Order& ord) {			//costruttore di copia
+
+Ordine::Ordine(const Ordine& ord) {			//costruttore di copia
 	time = ord.time;
 	id = ord.id;
 	quantity = ord.quantity;
 }
-Order::Order(Order&& ord) {				//costruttore di spostamento
+Ordine::Ordine(Ordine&& ord) {				//costruttore di spostamento
 	time = ord.time;
 	id = ord.id;
 	quantity = ord.quantity;
@@ -32,26 +30,25 @@ Order::Order(Order&& ord) {				//costruttore di spostamento
 	ord.id = 0;
 	ord.quantity = 0;
 }
-Order::~Order() {					//distruttore
+Ordine::~Ordine() {							//distruttore
 	time = 0;
 	id = 0;
 	quantity = 0;
 }
-//metodi get della classe Order
-int Order::getId()const { return id; }
-int Order::getTime()const { return time; }
-int Order::getQuantity()const { return quantity; }
-Model Order::getModel()const {return mod;}
+//metodi get della classe ordini
+int Ordine::getId()const { return id; }
+int Ordine::getTime()const { return time; }
+int Ordine::getQuantity()const { return quantity; }
 
 //overload operatore = per copia
-Order& Order::operator=(const Order& ord) {
+Ordine& Ordine::operator=(const Ordine& ord) {
 	time = ord.time;
 	id = ord.id;
 	quantity = ord.quantity;
 	return *this;
 }
 //overload operatore = per spostamento
-Order& Order::operator=(Order&& ord) {
+Ordine& Ordine::operator=(Ordine&& ord) {
 	time = ord.time;
 	id = ord.id;
 	quantity = ord.quantity;
@@ -60,7 +57,39 @@ Order& Order::operator=(Order&& ord) {
 	ord.quantity = 0;
 	return *this;
 }
+bool Ordine::operator<(const Ordine& o) {
+	return (time < o.time);
+}
 //overload operatore <<
-ostream& operator<<(ostream os,const Order ord) {
+ostream& operator<<(ostream os,const Ordine ord) {
 	os << "time= [" << ord.getTime() << "]\n" << "id= [" << ord.getId() << "]\n" << "quantity= [" << ord.getQuantity() << "]\n";
+}
+Ordine::Ordine(string nome_)
+{
+	string* arr = new string[3];
+	arr = divide(nome_);
+
+	time = stoi(arr[0]);
+	id= stoi(arr[1]);
+	quantity = stoi(arr[2]);
+	delete[] arr;
+}
+string* divide(string s)                              //divide le stringhe caricate
+{
+	string a = "";
+	int j = 0;
+	string* arr = new string[3];
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s.at(i) == '[')
+			a = "";
+		else if (s.at(i) == ']')
+		{
+			arr[j] = a;
+			j++;
+		}
+		else
+			a += s.at(i);
+	}
+	return arr;
 }
