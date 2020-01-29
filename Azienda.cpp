f0f06c9f2c40538ec5a3e7ed8bfa6e199d4353d4
matteo <matteo99moro@gmail.com>
2020-01-29 21:47:06 +0100

    robe cattivissime
    dcfs

--- Azienda.cpp
+++ Azienda.cpp
@@ -179,3 +179,28 @@ void Azienda::carica_modelli()
 	}
 }
 
+
+/** get_acquired_components() */
+void Azienda::get_acquired_component(){
+    for(int i=0; i<comp_acquistati.size(); i++)
+        Model m= (comp_acquistati)[i].getModel();
+    vector<Components> comp= m.getComponents();
+    for(int j=0; j<comp.size(); j++)
+        //stampa id, nome, .... std::cout <<
+        }//get_acquired_components
+
+/** get_warehouse() */
+void Azienda::get_warehouse(){
+    for(int i=0; i<magazzino.size(); i++)
+        Components comp= warehouse[i].getComp();
+    //stampa id, nome, ... std::cout <<
+}//get_warehouse
+
+/** get_delivered_orders() */
+void Azienda::get_delivered_orders(){
+    for(int i=0; i<ordini_evasi.size(); i++)
+        Model m= (ordini_evasi)[i];
+    //stampa id, nome, .... std::cout <<
+}//get_delivered_orders
+
+
